cc3b985b5ca887a04f5031ff9c2d041e4015507b
Rodrigo Villegas <rodrigo.villegas.venegas@gmail.com>
2021-11-03 15:16:10 -0400

    ok

--- Lecture/Assignment03/Assignment03_06.py
+++ Lecture/Assignment03/Assignment03_06.py
@@ -2,7 +2,7 @@ numList = []
 for i in range(10):
     num = int(input("Enter a number: "))
     numList.append(num)
-print("Low: ", float(min(numList)))
-print("High: ", float(max(numList)))
-print("Total: ", float(sum(numList)))
-print("Average: ", float(sum(numList))/len(numList))
+print("Low: ", float(min(numList), "\n",
+      "High: ", float(max(numList))), "\n",
+      "Total: ", float(sum(numList)), "\n",
+      "Average: ", float(sum(numList))/len(numList))
